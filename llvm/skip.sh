f5f0e29f3396d14e0ac73b9bc58b85d943ba457d
Robin Eklind <rnd0x00@gmail.com>
2018-11-21 23:21:27 +0100

    llvm: skip tests that should and do report errors
    	# should be invalid syntax
    
    	- [ ] Assembler/2003-11-11-ImplicitRename.ll
    		- note: function named %test
    		- got: syntax error at line 3
    
    	- [ ] Assembler/2004-03-30-UnclosedFunctionCrash.ll
    		- expected: found end of file when expecting more instructions
    		- got: syntax error at line 4
    
    	- [ ] Assembler/2007-08-06-AliasInvalid.ll
    		- expected: expected top-level entity
    		- got: syntax error at line 6
    
    	# should report error
    
    	- [ ] Assembler/2003-11-24-SymbolTableCrash.ll
    		- expected: multiple definition
    		- got: local identifier "%tmp.1" already present; prev `i32 %tmp.1`, new `i32 %tmp.1`
    
    	- [ ] Assembler/2004-11-28-InvalidTypeCrash.ll
    		- expected: use of undefined type named 'struct.none'
    		- got: unable to locate type definition of named type "%struct.none"
    
    	- [ ] Assembler/2006-09-28-CrashOnInvalid.ll
    		- expected: floating point constant invalid for type
    		- got: invalid type of floating-point constant; expected *types.FloatType, got *types.IntType
    
    	- [ ] Assembler/2007-01-02-Undefined-Arg-Type.ll
    		- expected: use of undefined type named 'typedef.bc_struct'
    		- got: unable to locate type definition of named type "%typedef.bc_struct"
    
    	- [ ] Assembler/2007-11-26-AttributeOverload.ll
    		- expected: an error
    		- got: global identifier "@atoi" already present; prev `declare i32 @atoi(i8*) nounwind readonly
    
    	- [ ] Assembler/2008-02-18-IntPointerCrash.ll
    		- expected: integer constant must have integer type
    		- got: invalid type of integer constant; expected *types.IntType, got *types.PointerType

---
+++ llvm/skip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mv "test/Assembler/2003-11-11-ImplicitRename.ll" "test/Assembler/2003-11-11-ImplicitRename.ll.x"
+mv "test/Assembler/2004-03-30-UnclosedFunctionCrash.ll" "test/Assembler/2004-03-30-UnclosedFunctionCrash.ll.x"
+mv "test/Assembler/2003-11-24-SymbolTableCrash.ll" "test/Assembler/2003-11-24-SymbolTableCrash.ll.x"
+mv "test/Assembler/2004-11-28-InvalidTypeCrash.ll" "test/Assembler/2004-11-28-InvalidTypeCrash.ll.x"
+mv "test/Assembler/2006-09-28-CrashOnInvalid.ll" "test/Assembler/2006-09-28-CrashOnInvalid.ll.x"
+mv "test/Assembler/2007-01-02-Undefined-Arg-Type.ll" "test/Assembler/2007-01-02-Undefined-Arg-Type.ll.x"
+mv "test/Assembler/2007-08-06-AliasInvalid.ll" "test/Assembler/2007-08-06-AliasInvalid.ll.x"
+mv "test/Assembler/2007-11-26-AttributeOverload.ll" "test/Assembler/2007-11-26-AttributeOverload.ll.x"
+mv "test/Assembler/2008-02-18-IntPointerCrash.ll" "test/Assembler/2008-02-18-IntPointerCrash.ll.x"
